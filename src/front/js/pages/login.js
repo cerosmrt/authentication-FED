335ca76a62718c7ad2787f66db82271652a3de4f
Federico Moret <106476837+shlimsady@users.noreply.github.com>
2024-06-28 05:18:39 +0000

    signup needs doing

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const onSubmit = async () => {
+        if(email === '' || password === '') {
+            alert("Email or Password should not be empty");
+        } else {
+            fetch(process.env.BACKEND_URL + "/api/log-in", {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                }),
+                headers: {
+                    'Content-Type': 'application/json'},
+            }).then((res) => res.json())
+            .then((resAsJson) => {
+                console.log('Response from Backend', resAsJson);
+                localStorage.setItem('jwt-token', resAsJson.token);
+                navigate('/');
+            }).catch((err) => {
+                console.error('Something wrong when calling API', err)
+            })               
+        }
+    }
+
+
+    return (
+        <div className="container">
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                <input 
+                    type="email" 
+                    className="form-control" 
+                    id="exampleFormControlInput1" 
+                    placeholder="name@example.com" 
+                    value={email}
+                    onChange = {(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input 
+                    type="password" 
+                    className="form-control" 
+                    id="exampleInputPassword1"
+                    value={password}
+                    onChange = {(e) => setPassword(e.target.value)}
+                />
+                  <button type="submit" className="btn btn-primary" onClick={onSubmit}>Submit</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
